8ea7202b45a682eb6cdf4baff62b988a616a9cde
Yang Zhao <freda.y.zhao@gmail.com>
2022-06-01 21:29:59 -0400

    branch01

---
+++ src/Quiz.svelte
@@ -0,0 +1,10 @@
+<script>
+  export let quizName = "Scott Quiz";
+  let title = "";
+</script>
+
+<div>
+  <h2>{quizName}</h2>
+  <h4>{title}</h4>
+  <input bind:value={title} type="text" />
+</div>
