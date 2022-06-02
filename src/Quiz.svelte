d54b24a1e696f735731fc1c42b2a6ed43c9327a1
Yang Zhao <freda.y.zhao@gmail.com>
2022-06-02 18:11:04 -0400

    update

--- src/Quiz.svelte
+++ src/Quiz.svelte
@@ -1,8 +1,9 @@
 <script>
   let result = "";
   let correctAnswer = "b";
-
   let answers = ["a", "b", "c", "d"];
+  let quiz = getQuiz();
+
   function pickAnswer(answer) {
     if (answer === correctAnswer) {
       result = "Correct!";
@@ -10,14 +11,37 @@
       result = "OOPS";
     }
   }
+
+  async function getQuiz() {
+    const res = await fetch(
+      "https://opentdb.com/api.php?amount=10&category=25&type=multiple"
+    );
+    const quiz = await res.json();
+    return quiz;
+    // debugger;
+  }
+
+  function handleClick() {
+    quiz = getQuiz();
+  }
 </script>
 
 <div>
+  <button on:click={handleClick}>Get Questions </button>
   {#if result}
     <h4>{result}</h4>
   {:else}
     <h5>Please pick an answer</h5>
   {/if}
+
+  {#await quiz}
+    Loading...
+  {:then data}
+    <h3>
+      {data.results[0].question}
+    </h3>
+  {/await}
+
   {#each answers as answer}
     <button on:click={() => pickAnswer(answer)}
       >Answer {answer.toUpperCase()}
