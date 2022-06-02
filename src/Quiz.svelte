a3b8151039b5dc9d7bf034eaae1e9555318d4433
Yang Zhao <freda.y.zhao@gmail.com>
2022-06-01 21:32:47 -0400

    calculator

--- src/Quiz.svelte
+++ src/Quiz.svelte
@@ -1,10 +1,14 @@
 <script>
   export let quizName = "Scott Quiz";
-  let title = "";
+  let a = 0;
+  let b = 0;
+  //   let title = "";
 </script>
 
 <div>
   <h2>{quizName}</h2>
-  <h4>{title}</h4>
-  <input bind:value={title} type="text" />
+
+  <input bind:value={a} type="number" />
+  <input bind:value={b} type="number" />
+  <h4>{a + b}</h4>
 </div>
