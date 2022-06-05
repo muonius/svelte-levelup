c169ecf406a9ee1309c87cd0427ee77f0975c3b6
Yang Zhao <freda.y.zhao@gmail.com>
2022-06-05 13:40:24 -0400

    update

--- src/Modal.svelte
+++ src/Modal.svelte
@@ -18,7 +18,7 @@
   </div>
 </div>
 
-<style>
+<style lang="scss">
   .modal-bg {
     position: fixed;
     top: 0;
@@ -26,11 +26,10 @@
     right: 0;
     bottom: 0;
     background: rgba(0, 0, 0, 0.8);
-  }
-
-  .modal {
-    background: white;
-    padding: 20px;
-    border-radius: 15px;
+    .modal {
+      background: white;
+      padding: 20px;
+      border-radius: 15px;
+    }
   }
 </style>
